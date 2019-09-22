46df0de775725153c9298f1881bec0ad315729b3
Natale Toscano <kickalliance195@gmail.com>
2019-09-22 16:46:18 -0400

    Complete the code skeleton of the program; Plays 5 rounds

---
+++ scripts/script.js
@@ -0,0 +1,43 @@
+let choices = ["Rock", "Paper", "Scissors"];
+let computerPlay = () => choices[Math.floor(Math.random()*3)];
+function play() {
+    let computerChoice = computerPlay();
+    let playerChoice;
+    while(choices.indexOf(playerChoice) == -1){
+        playerChoice = prompt("Rock, Paper, Scissors\nWhat do you choose?").toLowerCase();
+        playerChoice = playerChoice[0].toUpperCase() + playerChoice.substring(1,);
+    }
+
+    console.log("You chose " + playerChoice + " and the computer chose " + computerChoice + ".");
+
+    switch(computerChoice) {
+        case choices[0]:
+            if(choices[0] == playerChoice) outcome(0);
+            else choices[1] == playerChoice ? outcome(1) : outcome(-1);
+            break;
+        case choices[1]:
+            if(choices[1] == playerChoice) outcome(0);
+            else choices[2] == playerChoice ? outcome(1) : outcome(-1);
+            break;
+        case choices[2]:
+            if(choices[2] == playerChoice) outcome(0);
+            else choices[0] == playerChoice ? outcome(1) : outcome(-1);
+            break;
+    }
+}
+
+function game() {
+    for(let i=0; i<5; i++)
+        play();
+}
+
+function outcome(num) {
+    if(num==0)
+        console.log("Draw");
+    else if(num==1)
+        console.log("Win!");
+    else if(num==-1)
+        console.log("You lose!!");
+}
+
+game();
